ff7b0cd818852149ee8ea1cdc5e0a991e430b89a
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-11 02:41:13 +0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.paysync.gateway"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.6"
     }
     buildTypes {
         release {
